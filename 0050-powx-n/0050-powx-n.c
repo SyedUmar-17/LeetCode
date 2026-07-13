b9a36087a54fbcecc38b85d8d315c2f8cdf141f8
Syed Umar <1nc24ci111@ncetmail.com>
2026-07-13 21:07:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (78.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,21 @@
+double myPow(double x, int n) {
+    long long power = n;
+    double ans = 1.0;
+
+    if (power < 0) {
+        x = 1 / x;
+        power = -power;
+    }
+
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            ans = ans * x;
+        }
+
+        x = x * x;
+        power = power / 2;
+    }
+
+    return ans;
+}
