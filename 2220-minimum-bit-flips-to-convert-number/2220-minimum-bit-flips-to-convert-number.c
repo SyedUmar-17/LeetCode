f4672229a184cfd77078da1e4d14c91ec904e76e
Syed Umar <1nc24ci111@ncetmail.com>
2026-07-16 21:32:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (81.51%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.c
@@ -0,0 +1,12 @@
+int minBitFlips(int start, int goal) {
+
+    int x = start ^ goal;
+    int count = 0;
+
+    while (x) {
+        x = x & (x - 1);
+        count++;
+    }
+
+    return count;
+}
