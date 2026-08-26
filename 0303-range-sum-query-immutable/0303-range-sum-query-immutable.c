8ad527ce73d7b6d94273147afe5f3cdcdd335c67
Syed Umar <1nc24ci111@ncetmail.com>
2026-08-26 12:57:31 +0530

    Time: 5 ms (19.48%), Space: 16.3 MB (35.96%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.c
@@ -0,0 +1,52 @@
+
+
+
+typedef struct {
+    int *prefix;
+    int size;
+} NumArray;
+
+
+NumArray* numArrayCreate(int* nums, int numsSize) {
+
+    NumArray* obj = malloc(sizeof(NumArray));
+
+    obj->size = numsSize;
+
+    obj->prefix = malloc(sizeof(int) * numsSize);
+
+    obj->prefix[0] = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        obj->prefix[i] =
+            obj->prefix[i - 1] + nums[i];
+    }
+
+    return obj;
+}
+
+
+int numArraySumRange(NumArray* obj, int left, int right) {
+
+    if (left == 0) {
+        return obj->prefix[right];
+    }
+
+    return obj->prefix[right]
+           - obj->prefix[left - 1];
+}
+
+
+void numArrayFree(NumArray* obj) {
+
+    free(obj->prefix);
+    free(obj);
+}
+
+/**
+ * Your NumArray struct will be instantiated and called as such:
+ * NumArray* obj = numArrayCreate(nums, numsSize);
+ * int param_1 = numArraySumRange(obj, left, right);
+ 
+ * numArrayFree(obj);
+*/
