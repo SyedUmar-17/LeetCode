ace732d9df13f7f21a87246cfde4720c6f0de095
Syed Umar <1nc24ci111@ncetmail.com>
2026-09-07 04:51:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (25%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.c
@@ -0,0 +1,15 @@
+bool isThree(int n) {
+    int i=1,count=0;
+    while(i<=n){
+        if(n%i==0){
+            count++;
+        }
+        i++;
+    }
+    if(count==3){
+        return true;
+    }else{
+        return false;
+    }
+
+}
