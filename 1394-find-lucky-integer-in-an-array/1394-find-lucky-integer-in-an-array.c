22c38f6d19915ee8671b151b08fd5800c71dde33
Syed Umar <1nc24ci111@ncetmail.com>
2026-09-17 19:14:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (36.22%) - LeetHub

---
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.c
@@ -0,0 +1,17 @@
+int findLucky(int* arr, int arrSize) {
+    int freq[501] = {0};
+
+    for (int i = 0; i < arrSize; i++) {
+        freq[arr[i]]++;
+    }
+
+    int answer = -1;
+
+    for (int i = 1; i <= 500; i++) {
+        if (freq[i] == i) {
+            answer = i;
+        }
+    }
+
+    return answer;
+}
